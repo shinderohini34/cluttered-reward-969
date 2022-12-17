324bc1395f494811d631889c626b8958562e0f7d
Rohini Shinde <shinderohini34@gmail.com>
2022-12-17 18:41:57 +0530

    update

--- orbitz-app/src/Home/Section4.jsx
+++ orbitz-app/src/Home/Section4.jsx
@@ -7,15 +7,15 @@ export default function Section4()
         <Box maxW="7xl" mx={'auto'} pt={3} px={{ base: 2, sm: 12, md: 17 }}>
             <SimpleGrid columns={2} spacing={5} className="section4images">
                 <Box className="img1">
-                    <Heading pt={20}>National Parks</Heading>
-                    <Text>There's plenty to keep you busy.</Text>
-                    <Button color={"blue"} bg={"white"} size='sm' mt={3}>Button</Button>
+                    <Heading pt={20} ml={"10"}>National Parks</Heading>
+                    <Text ml={"10"}>There's plenty to keep you busy.</Text>
+                    <Button ml={"10"} color={"blue"} bg={"white"} size='sm' mt={3}>Button</Button>
 
                 </Box>
                 <Box className="img2">
-                    <Heading pt={20}>The perfect road trip</Heading>
-                    <Text>Find an adventure for every season</Text>
-                    <Button color={"blue"} bg={"white"} size='sm' mt={3}>Button</Button>
+                    <Heading ml={"10"} pt={20}>The perfect road trip</Heading>
+                    <Text ml={"10"}>Find an adventure for every season</Text>
+                    <Button ml={"10"} color={"blue"} bg={"white"} size='sm' mt={3}>Button</Button>
                 </Box>
             </SimpleGrid>
         </Box>
